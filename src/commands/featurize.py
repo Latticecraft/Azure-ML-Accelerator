662f8099b1811e9de0285fb3dba5217db80c9f3a
steve.nelson <snlsn@pm.me>
2022-07-05 20:18:13 -0700

    fixing bug where project not passed in log_metrics

--- src/commands/featurize.py
+++ src/commands/featurize.py
@@ -14,16 +14,6 @@ def main(args):
                 'type': 'uri_file',
                 'path': 'azureml://datastores/input/paths/placeholder'
             },
-            'runinfo': {
-                'type': 'uri_folder',
-                'path': 'azureml://datastores/output/paths/placeholder',
-                'mode': 'ro_mount'
-            },
-            'trainlog': {
-                'type': 'uri_folder',
-                'path': 'azureml://datastores/output/paths/placeholder',
-                'mode': 'ro_mount'
-            },
             'project': 'None',
             'type': 'None',
             'separator': 'comma',
@@ -131,20 +121,17 @@ def main(args):
                     'unwanted_csv': '${{parent.jobs.select_unwanted_job.outputs.transformed_data}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'split_dataset_job': {    
                 'type': 'command',
                 'component': 'file:../../config/component/split_dataset.yaml',
                 'inputs': {
-                    'datasets_pkl': '${{parent.jobs.package_job.outputs.transformed_data}}',
-                    'metrics_in': '${{parent.jobs.package_job.outputs.metrics_out}}'
+                    'datasets_pkl': '${{parent.jobs.package_job.outputs.transformed_data}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'downsample_job': {
@@ -156,56 +143,47 @@ def main(args):
                     'ratio': '${{parent.inputs.downsample_ratio}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'dfs_job': {
                 'type': 'command',
                 'component': 'file:../../config/component/dfs.yaml',
                 'inputs': {
-                    'datasets_pkl': '${{parent.jobs.downsample_job.outputs.transformed_data}}',
-                    'metrics_in': '${{parent.jobs.downsample_job.outputs.metrics_out}}'
+                    'datasets_pkl': '${{parent.jobs.downsample_job.outputs.transformed_data}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'stats_job': {
                 'type': 'command',
                 'component': 'file:../../config/component/stats.yaml',
                 'inputs': {
-                    'datasets_pkl': '${{parent.jobs.dfs_job.outputs.transformed_data}}',
-                    'metrics_in': '${{parent.jobs.dfs_job.outputs.metrics_out}}'
+                    'datasets_pkl': '${{parent.jobs.dfs_job.outputs.transformed_data}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'impute_job': {
                 'type': 'command',
                 'component': 'file:../../config/component/impute.yaml',
                 'inputs': {
-                    'datasets_pkl': '${{parent.jobs.stats_job.outputs.transformed_data}}',
-                    'metrics_in': '${{parent.jobs.stats_job.outputs.metrics_out}}'
+                    'datasets_pkl': '${{parent.jobs.stats_job.outputs.transformed_data}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'outliers_job': {
                 'type': 'command',
                 'component': 'file:../../config/component/outliers.yaml',
                 'inputs': {
-                    'datasets_pkl': '${{parent.jobs.impute_job.outputs.transformed_data}}',
-                    'metrics_in': '${{parent.jobs.impute_job.outputs.metrics_out}}'
+                    'datasets_pkl': '${{parent.jobs.impute_job.outputs.transformed_data}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'balancer_job': {
@@ -213,12 +191,10 @@ def main(args):
                 'component': 'file:../../config/component/balancer.yaml',
                 'inputs': {
                     'datasets_pkl': '${{parent.jobs.outliers_job.outputs.transformed_data}}',
-                    'type': '${{parent.inputs.type}}',
-                    'metrics_in': '${{parent.jobs.outliers_job.outputs.metrics_out}}'
+                    'type': '${{parent.inputs.type}}'
                 },
                 'outputs': {
-                    'transformed_data': {},
-                    'metrics_out': {}
+                    'transformed_data': {}
                 }
             },
             'register_dataset_job': {
@@ -246,6 +222,7 @@ def main(args):
                 'component': 'file:../../config/component/log_metrics.yaml',
                 'inputs': {
                     'datasets_pkl': '${{parent.jobs.balancer_job.outputs.transformed_data}}',
+                    'project': '${{parent.inputs.project}}',
                     'destination_folder': 'runinfo'
                 },
                 'outputs': {
@@ -263,7 +240,7 @@ def main(args):
         yaml.safe_dump(template, f, sort_keys=False,  default_flow_style=False)
 
     if eval(args.run) == True:
-        command = f'az ml job create --file {filepath} --web --set inputs.project={args.project} --set inputs.type={args.type} --set inputs.input_csv.path=azureml://datastores/input/paths/{args.project}/{args.input} --set inputs.runinfo.path=azureml://datastores/output/paths/{args.project}/runinfo --set inputs.trainlog.path=azureml://datastores/output/paths/{args.project}/trainlog --set experiment_name={args.project} --set inputs.label={args.label} --set inputs.unwanted={args.unwanted} --set inputs.replacements={args.replacements} --set inputs.datatypes={args.datatypes} --set inputs.separator={args.separator} --set inputs.web_hook="{args.web_hook}" --set inputs.next_pipeline={args.next_pipeline}'
+        command = f'az ml job create --file {filepath} --web --set inputs.project={args.project} --set inputs.type={args.type} --set inputs.input_csv.path=azureml://datastores/input/paths/{args.project}/{args.input} --set experiment_name={args.project} --set inputs.label={args.label} --set inputs.unwanted={args.unwanted} --set inputs.replacements={args.replacements} --set inputs.datatypes={args.datatypes} --set inputs.separator={args.separator} --set inputs.web_hook="{args.web_hook}" --set inputs.next_pipeline={args.next_pipeline}'
         print(f'command: {command}')
 
         list_files = subprocess.run(command.split(' '))
